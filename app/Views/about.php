8db5c97a56766a1d0c711d778e54999acbb710e7
JBA19 <jbacorrtech@gmail.com>
2021-05-08 19:00:56 +0200

    MAJ 08/05

--- app/Views/about.php
+++ app/Views/about.php
@@ -12,7 +12,7 @@
     </main>
 </div> -->
 <main id="main" class="container-fluid">
-<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel" data-bs-interval="false">
   <div class="carousel-indicators">
     <span type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1">1986</span>
     <span type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2">2004</span>
@@ -21,34 +21,32 @@
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="https://www.cieletespace.fr/media/default/0001/17/ESA_Gaia_DR2_1400x700-5c75.jpeg" class="d-block" alt="...">
-      <div class="carousel-caption d-none d-md-block animate__animated animate__backInRight">
+      <div class="carousel-caption d-none d-md-block text-end animate__animated animate__backInRight">
         <h5>First slide label</h5>
         <p>Some representative placeholder content for the first slide.</p>
       </div>
     </div>
     <div class="carousel-item">
       <img src="https://www.cieletespace.fr/media/default/0001/17/ESA_Gaia_DR2_1400x700-5c75.jpeg" class="d-block" alt="...">
-      <div class="carousel-caption d-none d-md-block animate__animated animate__backInRight">
+      <div class="carousel-caption d-none d-md-block text-end animate__animated animate__backInRight">
         <h5>Second slide label</h5>
         <p>Some representative placeholder content for the second slide.</p>
       </div>
     </div>
     <div class="carousel-item">
       <img src="https://www.cieletespace.fr/media/default/0001/17/ESA_Gaia_DR2_1400x700-5c75.jpeg" class="d-block" alt="...">
-      <div class="carousel-caption d-none d-md-block animate__animated animate__backInRight">
+      <div class="carousel-caption d-none d-md-block text-end animate__animated animate__backInRight">
         <h5>Third slide label</h5>
         <p>Some representative placeholder content for the third slide.</p>
       </div>
     </div>
   </div>
-  <!-- <li class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+  <li class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="">Previous</span>
   </li>
   <li class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="">Next</span>
-  </li> -->
+  </li>
 </div>
 </main>
 <?= $this->endsection("body") ?>
