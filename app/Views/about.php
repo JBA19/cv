c270870dc38d81593fbac8b8ad2ebf1e1c78ce9e
JBA19 <jbacorrtech@gmail.com>
2021-05-20 15:33:27 +0200

    MAJ 20/05

--- app/Views/about.php
+++ app/Views/about.php
@@ -1,58 +1,81 @@
 <?= $this->extend("app") ?>
 <?= $this->section("body") ?>
-<!-- 
-    <main id="main" class="row">
-        <section>
-            <h1>A propos de moi !</h1>
-            <p>Grâce à une formation, je me suis connecté au monde du développement web depuis fin 2018.</p>
-            <p>Initié aux technologies à la fois front et back end, j'ai également été présenté aux certifications des méthodes Agiles ainsi qu'aux bonnes pratiques OPQUAST.</p>
-            <p>Je suis actuellement à la recherche d'un poste qui me permettra de parfaire les connaissances acquises et d'explorer toujours plus les technologies actuelles.</p>
-            <p>Rigoureux et curieux, je suis globalement passionné de sciences, et plus particulièrement l'astro-physique. Sportif, je pratique le basket-ball depuis mon adolescence, sport que j'affectionne pour son aspect stratégique et la cohésion de groupe.</p>
-        </section>
-    </main>
-</div> -->
+
 <main id="main" class="container-fluid">
-  <div id="carouselExampleCaptions" class=" carousel slide" data-bs-ride="carousel" data-bs-interval="false">
+  <h1>A propos de moi !</h1>
+  <div id="timeline-carousel" class=" carousel slide" data-bs-ride="carousel" data-bs-interval="false">
     <div class="carousel-indicators">
-      <span type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1">1986</span>
-      <span type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2">2004</span>
-      <span type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3">2018</span>
+      <span type="button" data-bs-target="#timeline-carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1">1986</span>
+      <span type="button" data-bs-target="#timeline-carousel" data-bs-slide-to="1" aria-label="Slide 2">2004</span>
+      <span type="button" data-bs-target="#timeline-carousel" data-bs-slide-to="2" aria-label="Slide 3">2009</span>
+      <span type="button" data-bs-target="#timeline-carousel" data-bs-slide-to="3" aria-label="Slide 4">2010</span>
+      <span type="button" data-bs-target="#timeline-carousel" data-bs-slide-to="4" aria-label="Slide 5">2010-2012</span>
+      <span type="button" data-bs-target="#timeline-carousel" data-bs-slide-to="5" aria-label="Slide 6">2012-2014</span>
+      <span type="button" data-bs-target="#timeline-carousel" data-bs-slide-to="6" aria-label="Slide 7">2016-2018</span>
+      <span type="button" data-bs-target="#timeline-carousel" data-bs-slide-to="7" aria-label="Slide 8">2018-2021</span>
     </div>
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src="https://www.cieletespace.fr/media/default/0001/17/ESA_Gaia_DR2_1400x700-5c75.jpeg" class="d-block" alt="...">
+        <img src="<?php echo base_url(); ?>/img/1986.jpg" class="d-block w-100 w-100" alt="naissance">
         <div class="carousel-caption d-none d-md-block text-end animate__animated animate__backInRight">
-          <h5>First slide label</h5>
-          <p>Some representative placeholder content for the first slide.</p>
+        <div>
+          <h2>Venue au monde !</h2>
+          <p>Tout le mérite revient à ma mère,</p>
+          <p>mais cela reste quand même le point de départ !</p>
+        </div>  
         </div>
       </div>
       <div class="carousel-item">
-        <img src="https://www.cieletespace.fr/media/default/0001/17/ESA_Gaia_DR2_1400x700-5c75.jpeg" class="d-block" alt="...">
+        <img src="<?php echo base_url(); ?>/img/2004.jpeg" class="d-block w-100" alt="baccalauréat">
         <div class="carousel-caption d-none d-md-block text-end animate__animated animate__backInRight">
-          <h5>Second slide label</h5>
-          <p>Some representative placeholder content for the second slide.</p>
+          <h2>Baccalauréat Littéraire</h2>
+          <p>Options Mathématiques et LV3 Arabe, Lycée Saint Martin d'Angers.</p>
         </div>
       </div>
       <div class="carousel-item">
-        <img src="https://www.cieletespace.fr/media/default/0001/17/ESA_Gaia_DR2_1400x700-5c75.jpeg" class="d-block" alt="...">
+        <img src="<?php echo base_url(); ?>/img/2009.jpg" class="d-block w-100" alt="...Master droit international">
         <div class="carousel-caption d-none d-md-block text-end animate__animated animate__backInRight">
-          <h5>Third slide label</h5>
-          <p>Some representative placeholder content for the third slide.</p>
+          <h2>Master Droit International public</h2>
+          <p>Université de droit et de sciences politiques d'Angers.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="<?php echo base_url(); ?>/img/2010.jpg" class="d-block w-100" alt="Stage ONG">
+        <div class="carousel-caption d-none d-md-block text-end animate__animated animate__backInRight">
+          <h2>Stage auprès du CETIM</h2>
+          <p>Membre de la délégation représentative de l'ONG</p>
+          <p>lors de la sesssion du Conseil des Droits de l'Homme de l'ONU, Genève.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="<?php echo base_url(); ?>/img/2012.jpg" class="d-block w-100" alt="Secrétaire juridique Avoué">
+        <div class="carousel-caption d-none d-md-block text-end animate__animated animate__backInRight">
+          <h2>Secrétaire juridique</h2>
+          <p>SCP J-J BAZILLE, Avoué près de la Cour d'Appel de Rennes</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="<?php echo base_url(); ?>/img/2014.jpg" class="d-block w-100" alt="Master droit public">
+        <div class="carousel-caption d-none d-md-block text-end animate__animated animate__backInRight">
+          <h2>Master Droit public</h2>
+          <p>Université de droit et de sciences politiques de Rennes.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="<?php echo base_url(); ?>/img/2016.jpg" class="d-block w-100" alt="Clerc d'huissier">
+        <div class="carousel-caption d-none d-md-block text-end animate__animated animate__backInRight">
+          <h2>Clerc d'Huissier</h2>
+          <p>SCP SCHMITT-SOUIL, Huissiers de Justice en Corrèze.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="<?php echo base_url(); ?>/img/2021.png" class="d-block w-100" alt="Développeur web">
+        <div class="carousel-caption d-none d-md-block text-end animate__animated animate__backInRight">
+          <h2>Titre professionnel développeur web</h2>
+          <p>Formation auprès de la Corrèze Digital School, Tulle.</p>
         </div>
       </div>
     </div>
-    <!-- <div class="container-fluid col"> -->
-      <div class="row">
-          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-            <span><i class="fas fa-3x fa-angle-up"></i></span>
-          </button>
-      </div>
-      <div class="row">
-          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-            <span><i class="fas fa-3x fa-angle-down"></i></span>
-          </button>
-      </div>
-    <!-- </div> -->
   </div>
 </main>
 <?= $this->endsection("body") ?>
